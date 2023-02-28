2822fdf764be8efe55726242a1df6b93ba7ec24f
Aegizz <lloyddraysey@gmail.com>
2023-02-28 07:24:26 +0000

    correcting output for autograder

--- main.cpp
+++ main.cpp
@@ -4,6 +4,6 @@ extern int count(int[], int);
 
 int main() {
     int array[5] = {4,5,6,7,8};
-    std::cout << "The amount of even numbers is: " << count(array,5) << std::endl;
+    std::cout << "The number is: " << count(array,5) << std::endl;
     return 0;
 }
