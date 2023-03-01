7d26183868ec7acc9f4fc489a591a68a44da224a
root <root@boot2.com>
2023-03-02 01:04:45 +1030

    Final question  for this set of questions done

---
+++ function-1-5.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+int count_evens(int number){
+    if (number < 1){
+        return 0;
+    }
+    else {
+        int count =0;
+        for (int i = 1; i < number; i=i + 2){
+            count = count +1;
+        }
+        return count;
+
+    }
+}
--- main-1.cpp
+++ main-1.cpp
@@ -4,6 +4,8 @@ extern int array_sum(int[], int);
 extern double array_mean(int[], int);
 extern int num_count(int[], int, int);
 extern int sum_two_arrays(int[], int[], int);
+extern int count_evens(int);
+
 int main() {
     int array[5] = {1,2,3,4,5};
     int array2[5] = {5,4,3,2,1};
@@ -11,5 +13,5 @@ int main() {
     std::cout << "The average of the array is " << array_mean(array,5)<<std::endl;
     std::cout << "Parameter count is equal to " << num_count(array, 5, 3)<<std::endl;
     std::cout << "Sum of both arrays is: " << sum_two_arrays(array,array2, 5)<< std::endl;
-
+    std::cout << "Number of even numbers between parameter is " << count_evens(4)<<std::endl;
 }
