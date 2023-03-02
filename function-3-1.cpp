d81741d7f04ab0940456d19a2a9fe7a3dd3766b1
root <root@boot2.com>
2023-03-03 01:30:26 +1030

    Fixed 3-1

--- function-3-1.cpp
+++ function-3-1.cpp
@@ -5,24 +5,25 @@ bool is_fanarray(int array[], int n){
     if (n < 1){
         return 0;
     } else {
-        int backwards = n-1;
-        //loop through half of array check that it is same array backwards
-        for (int i =0; i <= backwards; i++){
 
+        int backwards = n-1;
+        //Loop check if the array is a palindrome
+        for (int i =0; i <= backwards; i++){
             if (array[i] != array[backwards]){
                 return 0;
             }
-            if (array[i - 1] > array[i]){
-                //return zero if the next element is smaller
-                return 0;
-            }
-            //Iterate other end backwards to check is palindrome
-
             backwards = backwards - 1;
-
         }
+        //Iterate other end backwards to check is ascending from one side
+        for (int i =0; i <= backwards; i++){
+            if (array[i-1] > array[i]){
+                    //return zero if the next element is smaller
+                    return 0;
+            }
+        }
+
         //return true if no return 0s
         return 1;
-        
     }
+        
 }
--- main-3-1.cpp
+++ main-3-1.cpp
@@ -4,7 +4,7 @@ extern bool is_fanarray(int[], int);
 
 
 int main() {
-    int array[5] = {2,4, 1,4,2};
+    int array[5] = {2,4, 4,2};
 
     std::cout << "Fan array bool is " << is_fanarray(array, 4)<< std::endl;
    
