930fa67054a0ea407b602c316c34910f65d2adb2
root <root@boot2.com>
2023-03-02 15:34:17 +1030

    Rest of medium problems

---
+++ function-2-4.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+bool is_ascending(int array[], int n){
+    if (n < 1) {
+        return 0;
+    } else {
+        for (int i = 1; i < n; i++){
+            if (array[i - 1] > array[i]){
+                return 0;
+            }
+        }
+        return 1;
+    }
+}
