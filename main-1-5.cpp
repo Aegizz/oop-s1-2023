3c4239b0f5ce4ec2fe06ea032355ab027e36eeaa
root <root@boot2.com>
2023-03-07 02:14:40 +1030

    Removed files from previous branch

--- main-1-5.cpp
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-
-
-extern int count_evens(int);
-
-int main() {
-    int array[5] = {1,2,3,4,5};
-    std::cout << "Number of even numbers between parameter is " << count_evens(1)<<std::endl;
-}
