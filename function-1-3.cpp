c2cfa5e1852ee664e1825e605ee0a4079a8f834f
root <root@boot2.com>
2023-03-07 13:48:18 +1030

    Part 1 Bug Fix

--- function-1-3.cpp
+++ function-1-3.cpp
@@ -42,7 +42,7 @@ void count_digits(int array[4][4]){
         
     }
     cout << "0:"<< zeros << ";1:" << ones << ";2:" << twos << ";3:"<<threes << ";4:" << fours << ";5:" << fives
-    << ";6:" << sixs << ";7:" << ";8:" <<eights << ";9:" << nines << endl;
+    << ";6:" << sixs << ";7:" << ";8:" <<eights << ";9:" << nines << ";"<< endl;
 
 
 }
--- function-1-4.cpp
+++ function-1-4.cpp
@@ -1,9 +1,13 @@
 #include <iostream>
 
-extern void print_scaled(int[3][3],int)
-
-int main(){
-    int scale = 3 ;
-    int threebythree[3][3] = {{0,1,2},{3,4,5},{6,7,8}};
-    print_scaled(threebythree,scale);
+using namespace std;
+void print_scaled(int array[3][3], int scale){
+    for (int i = 0; i < 3; i++){
+        for (int j =0; j < 3; j++){
+            array[i][j] = array[i][j] * 3;
+            cout << array[i][j] << " ";
+        }
+        cout << "\n";
+    }
+    cout << endl;
 }
