87edc521f596a3ebf61fa81a62c7201b5af1841d
root <root@boot2.com>
2023-03-09 13:30:08 +1030

    Bug fixes in 2-1

--- function-2-1.cpp
+++ function-2-1.cpp
@@ -16,13 +16,14 @@ void print_binary_str(string decimal_number){
     int deci_int  = stoi(decimal_number);
 
     int array[16] = {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0};
+    int deci_max =(16 - checkBitSize(deci_int));
     while (deci_int > 0){
         if (deci_int > 0){
             array[(16 - checkBitSize(deci_int))]++;
             deci_int = deci_int - pow(2, (checkBitSize(deci_int)-1));
         }
     }
-    for (int i=0; i < 16; i++){
+    for (int i= deci_max; i < 16; i++){
         cout << array[i];
     }
 
