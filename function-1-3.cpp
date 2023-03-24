284ffdfa70c2fd8bf0bac9aa2a70bcabf84e9a12
root <root@boot2.com>
2023-03-24 15:46:12 +1030

    Function-1-3

--- function-1-3.cpp
+++ function-1-3.cpp
@@ -1,12 +1,14 @@
 #include "Person.h"
 PersonList deepCopyPersonList(PersonList listPersons){
     PersonList newList;
-    int numberPeople;
-    Person* peopleList;
-    peopleList = listPersons.people;
-    numberPeople = listPersons.numPeople;
-    newList.numPeople = numberPeople;
-    newList.people = peopleList;
+    int n = listPersons.numPeople;
+    Person* array;
+    newList.people = new Person[n];
+    for (int i = 0; i < n; i++){
+        array[i] = listPersons.people[i];
+    }
+
+    newList.numPeople = n;
     
     return newList;
 }
