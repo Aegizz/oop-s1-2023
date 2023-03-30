3045cb6e640abe0d358dc7238718c5cfbb2e3852
root <root@boot2.com>
2023-03-30 12:50:22 +1030

    pracexam

---
+++ Bike.h
@@ -0,0 +1,16 @@
+// #ifndef
+#include <string>
+class Bike{
+    private:
+        std::string brand;
+        int code;
+    public:
+        Bike();
+        Bike(std::string brand, int code);
+        std::string get_brand();
+        int get_code();
+
+};
+
+
+// #endif
---
+++ main-1-1.cpp
@@ -0,0 +1,12 @@
+#include <string>
+#include <iostream>
+#include "Bike.h"
+using namespace std;
+
+int main(){
+    Bike Bike1 = Bike();
+    cout << Bike1.get_code() << "\n";
+    Bike Bike2 = Bike("Giant", 12345);
+    cout << Bike2.get_brand();
+
+}
