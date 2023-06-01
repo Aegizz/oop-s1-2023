e26f0ffbccab4038b5b6bacb490e2e748a78077f
lloyd <lloyd@LAPTOP-R5LT0PLV.modem>
2023-06-01 13:08:44 +0930

    exam

--- GridUnit.h
+++ GridUnit.h
@@ -2,16 +2,15 @@
 #define GRIDUNIT_H
 #include <tuple>
 
-using namespace std;
 
 class GridUnit{
     private:
-        tuple<int, int> coordinates;
+        std::tuple<int, int> coordinates;
         char entity;
     public:
         GridUnit();
         GridUnit(int x, int y, char entity);
-        tuple<int, int> getCoordinates();
+        std::tuple<int, int> getCoordinates();
         char getEntity();
         void setCoordinates(int x, int y);
         void setEntity(char entity);
@@ -20,18 +19,18 @@ class GridUnit{
 GridUnit::GridUnit(){
 
 };
-GridUnit::GridUnit(int _x, int _y, char _entity){
-    coordinates = tuple(_x, _y);
+GridUnit::GridUnit(int x, int y, char _entity){
+    coordinates = std::tuple(x, y);
     entity = _entity;
 };
 char GridUnit::getEntity(){
     return entity;
 }
-tuple<int, int> GridUnit::getCoordinates(){
+std::tuple<int, int> GridUnit::getCoordinates(){
     return coordinates;
 }
 void GridUnit::setCoordinates(int x, int y){
-    coordinates = tuple(x,y);
+    coordinates = std::tuple(x,y);
 }
 void GridUnit::setEntity(char _entity){
     entity = _entity;
--- main-1.cpp
+++ main-1.cpp
@@ -5,6 +5,6 @@
 using namespace std;
 int main(){
     GridUnit gridUnit = GridUnit(2,1,'a');
+    gridUnit.setCoordinates(5,3);
     cout << get<0>(gridUnit.getCoordinates());
-    Helper 
 }
