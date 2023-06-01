9156327cee01b335eaacabc468c9cb3abd170f3d
lloyd <lloyd@LAPTOP-R5LT0PLV.modem>
2023-06-01 12:44:43 +0930

    exam

--- Helper.cpp
+++ Helper.cpp
@@ -9,5 +9,5 @@ tuple<int, int> Helper::generateRandomCoordinates(int gridWidth, int gridHeight)
     return coords; 
 }
 double Helper::calculateDistance(tuple<int, int> coords1, tuple<int, int> coords2){
-    return sqrt(pow((coords1<0>-coords2<0>),2)+pow((coords1<1> - coords2<1>),2));
+    return sqrt(pow((get<0>(coords1)-get<0>(coords2)),2)+pow((get<1>(coords1) - get<1>(coords2)),2));
 }
