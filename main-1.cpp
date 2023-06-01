a32108028d1242c82c03e491ee4313c98e9acd7b
lloyd <lloyd@LAPTOP-R5LT0PLV.modem>
2023-06-01 12:48:49 +0930

    exam

--- main-1.cpp
+++ main-1.cpp
@@ -1,7 +1,9 @@
 #include "Helper.h"
 #include "GridUnit.h"
 #include "Modifier.h"
-
+#include <iostream>
+using namespace std;
 int main(){
-
+    GridUnit gridUnit = GridUnit(2,1,'a');
+    cout << get<0>(gridUnit.getCoordinates());
 }
