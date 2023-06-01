7748fc62f96d9d8a417ff16d0f2ee48abfdea7ad
lloyd <lloyd@LAPTOP-R5LT0PLV.modem>
2023-06-01 13:31:57 +0930

    exam

--- Obstacle.h
+++ Obstacle.h
@@ -8,16 +8,28 @@ class Obstacle:public Modifier{
     private:
         bool active;
     public:
-        Obstacle(){};
-        Obstacle(int x, int y){
+        Obstacle();
+        Obstacle(int x, int y);
+        bool isActive();
+        void apply(GridUnit& unit);
+};
+
+Obstacle::Obstacle()
+{
+
+}
+Obstacle::Obstacle(int x, int y)
+{
     bool active = 1;
     coordinates = make_tuple(x, y);
-};
-        bool isActive(){        return active;
-};
-        void apply(GridUnit& unit){
+}
+
+bool Obstacle::isActive()
+{
+        return active;
+}
+void Obstacle::apply(GridUnit& unit){
     entity = 'O';
     active = 0;
-};
-};
+}
 #endif
