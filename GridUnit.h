3f3e42a31c9f2ebd8c79fceda7373af719acaba0
lloyd <lloyd@LAPTOP-R5LT0PLV.modem>
2023-06-01 13:10:39 +0930

    exam

--- GridUnit.h
+++ GridUnit.h
@@ -1,16 +1,16 @@
 #ifndef GRIDUNIT_H
 #define GRIDUNIT_H
 #include <tuple>
-
+using namespace std;
 
 class GridUnit{
     private:
-        std::tuple<int, int> coordinates;
+        tuple<int, int> coordinates;
         char entity;
     public:
         GridUnit();
         GridUnit(int x, int y, char entity);
-        std::tuple<int, int> getCoordinates();
+        tuple<int, int> getCoordinates();
         char getEntity();
         void setCoordinates(int x, int y);
         void setEntity(char entity);
@@ -20,17 +20,17 @@ GridUnit::GridUnit(){
 
 };
 GridUnit::GridUnit(int x, int y, char _entity){
-    coordinates = std::tuple(x, y);
+    coordinates = make_tuple(x, y);
     entity = _entity;
 };
 char GridUnit::getEntity(){
     return entity;
 }
-std::tuple<int, int> GridUnit::getCoordinates(){
+tuple<int, int> GridUnit::getCoordinates(){
     return coordinates;
 }
 void GridUnit::setCoordinates(int x, int y){
-    coordinates = std::tuple(x,y);
+    coordinates = make_tuple(x,y);
 }
 void GridUnit::setEntity(char _entity){
     entity = _entity;
